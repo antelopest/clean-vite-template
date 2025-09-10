9a999ff89c53c426bde0d9ae4f3856c9b6638580
antelopest <antelopest@gmail.com>
2025-09-10 18:26:03 +0700

    main changes with prettier

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/base/',
-    server: {
-        port: 3000,
-        host: true
-    },
-    css: {
-        postcss: './postcss.config.js'
-    }
+  base: '/base/',
+  server: {
+    port: 3000,
+    host: true
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
 });
